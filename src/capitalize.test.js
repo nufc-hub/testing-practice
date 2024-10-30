2a14879270fe41a27a97204edd7c80251a66fc75
nufc-hub <kmchenouf@gmail.com>
2024-10-30 22:29:35 +0900

    Write test for capitalize function

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -1,1 +1,11 @@
+import capitalize from './capitalize';
 
+// Check if first letter of string is capitalised.
+
+test('first letter is capitalized', () => {
+  expect(capitalize('test')).toBe('Test');
+});
+
+test('first letter is capitalized', () => {
+  expect(capitalize('road')).toBe('Road');
+});
