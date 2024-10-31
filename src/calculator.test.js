69fdbd4b4e771dfcfaf9fc9e10be3f3ce36797f3
nufc-hub <kmchenouf@gmail.com>
2024-10-31 22:05:39 +0900

    Add testing for divide

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -21,3 +21,13 @@ test('subtract 3 - 2 to equal 1', () => {
 test('subtract 4 - 4 to equal 0', () => {
   expect(calculator.subtract(4, 4)).toBe(0);
 });
+
+// Divide
+
+test('divide 2 by 1 to equal 2', () => {
+  expect(calculator.divide(2, 1)).toBe(2);
+});
+
+test('divide 12 by 3 to equal 4', () => {
+  expect(calculator.divide(12, 3)).toBe(4);
+});
