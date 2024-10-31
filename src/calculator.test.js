03cda59d457accbda1b003eb09932c848dccecc7
nufc-hub <kmchenouf@gmail.com>
2024-10-31 22:21:49 +0900

    Add testing for multiply function

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -24,10 +24,20 @@ test('subtract 4 - 4 to equal 0', () => {
 
 // Divide
 
-test('divide 2 by 1 to equal 2', () => {
+test('quotient of 2 and 1 to equal 2', () => {
   expect(calculator.divide(2, 1)).toBe(2);
 });
 
-test('divide 12 by 3 to equal 4', () => {
+test('quotient of 12 and 3 to equal 4', () => {
   expect(calculator.divide(12, 3)).toBe(4);
+});
+
+// Multiply.
+
+test('product of 1 and 2 to equal 2', () => {
+  expect(calculator.multiply(1, 2)).toBe(2);
+});
+
+test('product of 3 and 4 to equal 12', () => {
+  expect(calculator.multiply(3, 4)).toBe(12);
 });
