466f552851e39b23141610ebda9b9c48dac33f10
nufc-hub <kmchenouf@gmail.com>
2024-10-31 22:00:07 +0900

    Add testing for subtract

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -11,3 +11,13 @@ test('add 1 + 2 to equal 3', () => {
 test('add 2 + 2 to equal 4', () => {
   expect(calculator.sum(2, 2)).toBe(4);
 });
+
+// Subtract.
+
+test('subtract 3 - 2 to equal 1', () => {
+  expect(calculator.subtract(3, 2)).toBe(1);
+});
+
+test('subtract 4 - 4 to equal 0', () => {
+  expect(calculator.subtract(4, 4)).toBe(0);
+});
