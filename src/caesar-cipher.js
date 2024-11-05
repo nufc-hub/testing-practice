86a480d305b08fc5db1221e388bb8e588c00d0b9
nufc-hub <kmchenouf@gmail.com>
2024-11-05 20:51:53 +0900

    Add logic for character shift handling

--- src/caesar-cipher.js
+++ src/caesar-cipher.js
@@ -4,6 +4,23 @@ class CaesarCipher {
 
     for (let i = 0; i < string.length; i++) {
       let asciiCode = string.charCodeAt(i);
+
+      // Handle uppercase
+      if (asciiCode >= 65 && asciiCode <= 90) {
+        asciiCode = ((asciiCode - 65 + shiftFactor) % 26) + 65;
+        newString += String.fromCharCode(asciiCode);
+      }
+
+      // Handle lowercase
+      else if (asciiCode >= 97 && asciiCode <= 122) {
+        asciiCode = ((asciiCode - 97 + shiftFactor) % 26) + 97;
+        newString += String.fromCharCode(asciiCode);
+      }
+
+      // Handle non letter characters
+      else {
+        newString += String.fromCharCode(asciiCode);
+      }
     }
 
     return newString;
