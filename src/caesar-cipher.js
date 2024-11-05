821ff7ff77f8afc8757edd73af4e39acf2d395f0
nufc-hub <kmchenouf@gmail.com>
2024-11-05 20:51:09 +0900

    Add for loop

--- src/caesar-cipher.js
+++ src/caesar-cipher.js
@@ -2,6 +2,10 @@ class CaesarCipher {
   shift(string, shiftFactor) {
     let newString = '';
 
+    for (let i = 0; i < string.length; i++) {
+      let asciiCode = string.charCodeAt(i);
+    }
+
     return newString;
   }
 }
