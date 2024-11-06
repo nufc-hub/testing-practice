f2321c5eeee6b05eaf2a1c8942078f29b3d5b384
nufc-hub <kmchenouf@gmail.com>
2024-11-06 21:06:08 +0900

    Test array min value

--- src/analyzeArray.test.js
+++ src/analyzeArray.test.js
@@ -8,3 +8,11 @@ test('return average with value of 4', () => {
 
   expect(obj).toHaveProperty('average', 4);
 });
+
+// Test min
+
+test('return the lowest value in the array with a value of 1', () => {
+  const obj = arrayAnalyzer.createObject([1, 8, 3, 4, 2, 6]);
+
+  expect(obj).toHaveProperty('min', 1);
+});
