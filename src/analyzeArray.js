012d7a31c8c450fcdec24bd40331c8dd712287a4
nufc-hub <kmchenouf@gmail.com>
2024-11-06 16:18:37 +0900

    Add getAverage & createObject methods

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,1 +1,27 @@
-class ArrayAnalyzer {}
+class ArrayAnalyzer {
+  // Create object and set methods for getting the properties.
+  createObject(array) {
+    const obj = {
+      average: this.getAverage(array),
+      min: '',
+      max: '',
+      length: '',
+    };
+
+    return obj;
+  }
+
+  // Returns the array average.
+  getAverage(array) {
+    // Get sum of all numbers in array.
+    const sum = array.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    }, 0);
+
+    const average = sum / array.length;
+
+    return average;
+  }
+}
+
+export default ArrayAnalyzer;
