aba8b13772d6abeeea212422896bb9a27bff1238
nufc-hub <kmchenouf@gmail.com>
2024-11-06 21:23:39 +0900

    Add additional tests

--- src/analyzeArray.test.js
+++ src/analyzeArray.test.js
@@ -3,10 +3,17 @@ import ArrayAnalyzer from './analyzeArray';
 const arrayAnalyzer = new ArrayAnalyzer();
 
 // Test average.
+
 test('return average with value of 4', () => {
   const obj = arrayAnalyzer.createObject([1, 8, 3, 4, 2, 6]);
 
   expect(obj).toHaveProperty('average', 4);
+});
+
+test('return average with value of 5', () => {
+  const obj = arrayAnalyzer.createObject([2, 6, 3, 4, 5, 10]);
+
+  expect(obj).toHaveProperty('average', 5);
 });
 
 // Test min
@@ -15,4 +22,10 @@ test('return the lowest value in the array with a value of 1', () => {
   const obj = arrayAnalyzer.createObject([1, 8, 3, 4, 2, 6]);
 
   expect(obj).toHaveProperty('min', 1);
+});
+
+test('return the lowest value in the array with a value of 2', () => {
+  const obj = arrayAnalyzer.createObject([12, 8, 3, 4, 2, 6]);
+
+  expect(obj).toHaveProperty('min', 2);
 });
