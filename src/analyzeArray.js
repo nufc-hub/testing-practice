34cbff77a903487063564d230a7a02d0622111d7
nufc-hub <kmchenouf@gmail.com>
2024-11-06 21:31:22 +0900

    Add get max value functionality

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -2,9 +2,9 @@ class ArrayAnalyzer {
   // Create object and set methods for getting the properties.
   createObject(array) {
     const obj = {
-      average: this.getAverage(array),
-      min: this.getMin(array),
-      max: '',
+      average: this.getAverage(array), // The average value in the array.
+      min: this.getMin(array), // The min value in the array.
+      max: this.getMax(array), // The max value in the array.
       length: '',
     };
 
@@ -25,13 +25,24 @@ class ArrayAnalyzer {
 
   // Returns the min value in the array.
   getMin(array) {
-    // Sort the array.
+    // Sort the array - lowest to highest.
     const sortedArray = array.sort((a, b) => a - b);
 
     // Get the min value in the array.
     const minValue = sortedArray[0];
 
     return minValue;
+  }
+
+  // Returns the max value in the array.
+  getMax(array) {
+    // Sort the array - highest to lowest.
+    const sortedArray = array.sort((a, b) => b - a);
+
+    // Get the max value in the array.
+    const maxValue = sortedArray[0];
+
+    return maxValue;
   }
 }
 
